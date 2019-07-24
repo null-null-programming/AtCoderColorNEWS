65ab13bf03d5a38c32c42fb6b6d5a59f3a37bf2a
null_null <nullnullprogramming@gmail.com>
2019-07-24 22:11:35 +0900

    first commit

---
+++ AtCoderColorNEWS.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Atcoder__
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       nullnull
+// @match        https://atcoder.jp/?lang=ja
+// @require      https://cdn.rawgit.com/jaredreich/notie.js/a9e4afbeea979c0e6ee50aaf5cb4ee80e65d225d/notie.js
+// ==/UserScript==
+
+(function () {
+    //お気に入りリストを取得
+    let favList = localStorage.fav;
+    favList = favList.split('"');
+
+    //レートを色に変換するリスト
+    let color = ['灰', '茶', '緑', '水', '青', '黄', '橙', '赤', '自由'];
+
+    let string = ""; //通知用string
+    for (let i = 1; i < favList.length; i += 2) {
+        //favList[i]:=お気に入りの人のusername
+
+        let preRate = 1000;
+        let nowRate = 2500;
+        let joinFlag = true;
+        /*
+            TODO:
+            ・前回のコンテストのレートpreRateと今回のコンテストのレートnowRateを取得する
+            ・今回のコンテストに参加していない場合joinFlagをfalseにする
+        */
+
+        //参加していないのでcontinue
+        if (!joinFlag) continue;
+
+        //Rateを色に変換する
+        preRate = Math.floor(preRate / 400);
+        nowRate = Math.floor(nowRate / 400);
+
+        //前の色よりも今の色のほうが高い場合通知。
+        if (preRate < nowRate) {
+            string += favList[i] + 'さんのレートが' + color[nowRate] + '色に変わりました！<br>';
+        }
+    }
+
+    notie.alert(3, string, 20); //20秒後、またはクリックで消える
+})();
