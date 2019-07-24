ce42f2a3da7a51be8b3c844744bf93c92249d68f
keymoon <kymn0116@gmail.com>
2019-07-24 23:17:30 +0900

    implement fetch contest result

--- AtCoderColorNEWS.js
+++ AtCoderColorNEWS.js
@@ -3,32 +3,31 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @author       nullnull
-// @match        https://atcoder.jp/?lang=ja
+// @match        https://atcoder.jp
 // @require      https://cdn.rawgit.com/jaredreich/notie.js/a9e4afbeea979c0e6ee50aaf5cb4ee80e65d225d/notie.js
 // ==/UserScript==
 
-(function () {
+(async function () {
     //お気に入りリストを取得
     let favList = JSON.parse(localStorage.fav);
 
     //レートを色に変換するリスト
     let color = ['灰', '茶', '緑', '水', '青', '黄', '橙', '赤', '自由'];
-    
+
+    //直近のコンテスト
+    const latestContestScreenName = getLatestContestScreenName();
+
     let string = ""; //通知用string
-    favList.forEach(username => {
-        //favList[i]:=お気に入りの人のusername
+    favList.forEach(async username => {
+        const ratedHistoryData = (await getHistoryData(username)).filter(x => x.IsRated);
+        if (ratedHistoryData.length === 0) return;
 
-        let preRate = 1000;
-        let nowRate = 2500;
-        let joinFlag = true;
-        /*
-            TODO:
-            ・前回のコンテストのレートpreRateと今回のコンテストのレートnowRateを取得する
-            ・今回のコンテストに参加していない場合joinFlagをfalseにする
-        */
+        const latestHistory = ratedHistoryData.pop();
+        console.log(latestHistory);
+        if (latestHistory.ContestScreenName.split('.')[0] !== latestContestScreenName) return;
 
-        //参加していないのでcontinue
-        if (!joinFlag) continue;
+        let preRate = latestHistory.OldRating;
+        let nowRate = latestHistory.NewRating;
 
         //Rateを色に変換する
         preRate = Math.min(8, Math.floor(preRate / 400));
@@ -42,4 +41,13 @@
 
     notie.alert(3, string, 20); //20秒後、またはクリックで消える
 })();
-    
+
+function getLatestContestScreenName() {
+    //TODO:URLのabc123みたいな奴をとってくる
+    return "agc036";
+}
+
+async function getHistoryData(userScreenName) {
+    return await $.ajax(`https://atcoder.jp/users/${userScreenName}/history/json`)
+}
+
